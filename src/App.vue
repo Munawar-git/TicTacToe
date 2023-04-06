7055da2c0bcc21b7de0d3c0b57b467b6591ef846
Munawar <munawerali1993@gmail.com>
2023-04-06 23:45:13 +0200

    Tic Tac Toe game using Vue JS

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Tic Tac Toe</h1>
+    <div class="tictactoe">
+      <div>
+        <Box
+          v-for="n in 3"
+          v-bind:key="n"
+          @click.once="sendShape(n)"
+          :shape="shapeVal"
+          :index="n"
+          :win="win"
+        />
+      </div>
+      <div>
+        <Box
+          v-for="n in 3"
+          v-bind:key="n + 3"
+          @click.once="sendShape(n + 3)"
+          :shape="shapeVal"
+          :index="n + 3"
+          :win="win"
+        />
+      </div>
+      <div>
+        <Box
+          v-for="n in 3"
+          v-bind:key="n + 6"
+          @click.once="sendShape(n + 6)"
+          :shape="shapeVal"
+          :index="n + 6"
+          :win="win"
+        />
+      </div>
+    </div>
+    <h2 v-show="win">{{ win }} wins!!</h2>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Box from "./components/Box.vue";
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      shapeVal: true,
+      crossString: "",
+      zeroString: "",
+      win: null,
+      result: true,
+      winConditions: ["123", "456", "789", "147", "258", "369", "159", "357"],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Box,
+  },
+  methods: {
+    sendShape(index) {
+      this.checkString(this.shapeVal, index);
+      this.shapeVal = !this.shapeVal;
+      this.oldIndex = index;
+    },
+
+    checkString(value, index) {
+      if (value) {
+        this.crossString += String(index);
+        if (this.checkWinCondition(this.crossString)) {
+          this.win = "Blue";
+        }
+      } else {
+        this.zeroString += String(index);
+        if (this.checkWinCondition(this.zeroString)) {
+          this.win = "Green";
+        }
+      }
+      console.log(this.win);
+    },
+
+    checkWinCondition(checkString) {
+      console.log(checkString);
+      for (let i = 0; i < this.winConditions.length; i++) {
+        let singleCondition = [...this.winConditions[i]];
+        this.result =
+          checkString.includes(singleCondition[0]) &&
+          checkString.includes(singleCondition[1]) &&
+          checkString.includes(singleCondition[2]);
+        if (this.result) {
+          return this.result;
+        }
+      }
+      return this.result;
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +102,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.tictactoe {
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/Box.vue
@@ -0,0 +1,58 @@
+<template>
+  <button
+    class="box"
+    :class="{ blue: colorClass && color, green: colorClass && !color }"
+    @click="onClick"
+    :disabled="win"
+  >
+    <!-- {{ index }} -->
+    <p class="sign" v-show="colorClass && color">X</p>
+    <p class="sign" v-show="colorClass && !color">O</p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Box",
+  data() {
+    return {
+      isClicked: false,
+      colorClass: false,
+      color: false,
+    };
+  },
+  props: { shape: { type: Boolean }, index: Number, win: String },
+
+  methods: {
+    onClick() {
+      if (!this.isClicked) {
+        this.colorClass = true;
+        this.color = this.shape;
+        this.isClicked = true;
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.box {
+  display: block;
+  height: 50px;
+  width: 50px;
+  border: 2px solid red;
+  background: yellow;
+  cursor: pointer;
+}
+.box.blue {
+  background: blue;
+}
+.box.green {
+  background: green;
+}
+.sign {
+  color: white;
+  font-weight: bolder;
+  font-size: 1.25rem;
+}
+</style>
